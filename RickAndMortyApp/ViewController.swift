64cad176703f6b1192c09ff0034c1cf79d26dc87
Ahmer Mughal <114129424+ahmeramughal@users.noreply.github.com>
2023-05-18 12:24:29 +0500

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Ahmer Akhter on 18/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
